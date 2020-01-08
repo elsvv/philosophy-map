9a3da9d8106a2fbfd8132e989b38fe803690594f
elis_alech <elis.alech900@gmail.com>
2020-01-08 14:33:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Parser from "./containers/Parser/Parser";
+import axios from "axios";
+
+// const csrfToken = document.querySelector("meta[name=csrf-token]").content;
+// axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parser />
     </div>
   );
 }
